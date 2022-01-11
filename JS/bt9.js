337841fa42ce5f05a1d850c0b611feae032a68c3
Nguyen Thuy Tien <97055595+Dino-Nguyen@users.noreply.github.com>
2022-01-11 11:22:27 +0700

    Update bt9.js

--- JS/bt9.js
+++ JS/bt9.js
@@ -40,10 +40,11 @@ function FindMin() {
                 }
      // Nếu a và b bằng nhau
                 if (a == b) {
-                        alert("Two numbers are equal")
+                        alert("Two numbers are equal");
+                    return a;
+                    return b;
                 }
-      return a;
-      return b;
+      
     // so sánh a và b
     let result = (a < b) ? a : b;
     alert(result)
